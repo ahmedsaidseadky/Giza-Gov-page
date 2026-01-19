5965208f8855b6feeb1a4c3832865f6840df0610
ahmedsaidseadky <ahmedsaid123423@gmail.com>
2026-01-19 11:14:37 +0200

    Giza-gov-page_v1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // مهم جدا عشان GitHub Pages
+  output: 'export',
+
+  // اسم الريبو على GitHub
+  basePath: '/Giza-Gov-page',
+  assetPrefix: '/Giza-Gov-page/',
+
+  // صور Next
   images: {
     unoptimized: true,
   },
- 
-}
 
-export default nextConfig
+  // اختياري (انت حاطه بالفعل)
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
