dd2a0c9c2160f722c062bd5634d153182558025a
ahmedsaidseadky <ahmedsaid123423@gmail.com>
2026-01-19 11:13:39 +0200

    Updates

---
+++ _next/static/LqBlWeRsswfY94dXL4CPW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Giza-Gov-page//_next/:path+",
+        "destination": "/Giza-Gov-page/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
